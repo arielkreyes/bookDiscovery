fc3d5224f9516946c51fee4a2266ca1ada2e6273
arielkreyes <ariel.k.reyes@hotmail.com>
2017-03-06 11:56:25 -0800

    Rate
    Started rating page. Re-prioritze Project goal points. :)

---
+++ rate.php
@@ -0,0 +1,18 @@
+<?php
+require('db_config.php');
+require_once('functions.php');
+include('header.php');
+?>
+<main>
+  <figure class='rateSquare'>
+    <img src="BOOK"/>
+    <figcaption>
+      <div class="rangeslider"></div>
+      <p>RATE#</p>
+      <input type="button" value="+ Review"/>
+    </figcaption>
+  </figure>
+</main>
+<?php
+include('footer.php');
+?>
